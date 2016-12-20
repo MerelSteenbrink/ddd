3d3c2837186c97379397ab46ef2db75a2a163c64
MerelSteenbrink <merel.steenbrink@gmail.com>
2016-12-20 13:41:23 +0100

    add labels to nodes

--- firstgraph/script.js
+++ firstgraph/script.js
@@ -1,34 +1,45 @@
 // Select an element and add svg
 var vis = d3.select("#charity")
-            .append("svg");
+.append("svg");
 
-vis.attr("width", 900)
-  .attr("height", 400)
+vis.attr("width", 600)
+  .attr("height", 200)
   .classed("fillie", true)
 
-var nootjes = [{x: 30, y: 50},
-              {x: 190, y: 120},
-              {x: 390, y:50}]
 
-vis.selectAll("circle .nodes")
+var nootjes = [{x: 30, y: 50, name: "paprika"},
+{x: 190, y: 120, name: "bloemkool"},
+{x: 390, y:50, name: "spinazie"}]
+
+var pizza = vis.selectAll("circle .nodes")
     .data(nootjes)
     .enter().append("svg:circle")
     .attr("class", "nodes")
     .attr("cx", function(d) {return d.x})
     .attr("cy", function(d) {return d.y})
     .attr("r", 10)
-    .text("hello")
+
+
+vis.selectAll("text")
+    .data(nootjes)
+    .enter().append("text")
+    .attr("dx", function(d) {return d.x + 5})
+    .attr("dy", function(d) {return d.y -10})
+    .text(function(d) { return d.name })
+    .attr("font-family", "Verdana")
+    .attr("font-size", "20px")
+
 
 var linkies = [
-  {source: nootjes[0], target: nootjes[1]},
-  {source: nootjes[2], target: nootjes[1]}
+{source: nootjes[0], target: nootjes[1]},
+{source: nootjes[2], target: nootjes[1]}
 ]
 
 vis.selectAll(".line")
-  .data(linkies)
-  .enter().append("line")
-  .attr("x1", function(d) {return d.source.x})
-  .attr("y1", function(d) {return d.source.y})
-  .attr("x2", function(d) {return d.target.x})
-  .attr("y2", function(d) {return d.target.y})
-  .style("stroke", "rgb(6,120,155)");
+    .data(linkies)
+    .enter().append("line")
+    .attr("x1", function(d) {return d.source.x})
+    .attr("y1", function(d) {return d.source.y})
+    .attr("x2", function(d) {return d.target.x})
+    .attr("y2", function(d) {return d.target.y})
+    .style("stroke", "rgb(6,120,155)");
