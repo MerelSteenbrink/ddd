c41c3397a9aa09cd00fda953d2929f517b0126eb
MerelSteenbrink <merel.steenbrink@gmail.com>
2016-12-22 14:50:12 +0100

    little tweeks

--- circlefk/script.js
+++ circlefk/script.js
@@ -1,59 +1,63 @@
-var dataset = {
-"children": [
-{"name": "Donald Trump", "size": 6 },
-{"name": "Theresa May", "size": 64 },
-{"name": "Nicola Sturgeon", "size": 19 },
-{"name": "Philip Hammod", "size": 42 },
-{"name": "Angela Merkel", "size": 3 }
-]}
 
-var diameter = 800;
-    var color = d3.scaleOrdinal(d3.schemeCategory20);
+fetch("./bub.json")
+  .then(function(response) { return response.json()})
+  .then(function(d) { maak(d)});
 
-    var bubble = d3.pack(dataset)
-            .size([diameter, diameter])
-            .padding(1.5);
-    var svg = d3.select(".chart")
-            .append("svg")
-            .attr("width", diameter)
-            .attr("height", diameter)
-            .attr("class", "bubble");
+function maak(d) {
+  console.log(d);
+  var dataset = d;
+  visualization(d);
+}
 
-    var nodes = d3.hierarchy(dataset)
-            .sum(function(d) { return d.size; });
 
-    var node = svg.selectAll(".node")
-            .data(bubble(nodes).descendants())
-            .enter()
-            .filter(function(d){
-                return  !d.children
-            })
-            .append("g")
-            .attr("class", "node")
-            .attr("transform", function(d) {
-                return "translate(" + d.x + "," + d.y + ")";
-            });
+function visualization(dataset){
+  var svg = d3.select("svg"),
+      width = +svg.attr("width"),
+      height = +svg.attr("height");
 
-    node.append("title")
-            .text(function(d) {
-                return d.name + ": " + d.size;
-            });
 
-    node.append("circle")
-            .attr("r", function(d) {
-                return d.r;
-            })
-            .style("fill", function(d) {
-                return color(d.name);
-            });
 
-    node.append("text")
-            .attr("dy", ".3em")
-            .style("text-anchor", "middle")
-            .text(function(d) {
-                return d.data.name.substring(0, d.r / 3) + ": " + d.data.size;
-            });
+      var bubble = d3.pack(dataset)
+              .size([width/1.5, height /1.5])
+              .padding(3);
 
-    d3.select(self.frameElement)
-            .style("height", diameter + "px");
 
+      var nodes = d3.hierarchy(dataset)
+              .sum(function(d) { return d.times; });
+
+      var node = svg.selectAll(".node")
+              .data(bubble(nodes).descendants())
+              .enter()
+              .filter(function(d){
+                  return  !d.children
+              })
+              .append("g")
+              .attr("class", "node")
+              .attr("transform", function(d) {
+                  return "translate(" + d.x + "," + d.y + ")";
+              });
+
+
+      node.append("title")
+              .text(function(d) {
+                  return d.data.name + ": " + d.data.times;
+              });
+
+      node.append("circle")
+              .attr("r", function(d) {
+                  return d.r;
+              })
+              .style("fill", function(d) {
+                  return "#81BCB8";
+              });
+
+      node.append("text")
+              .attr("dy", ".3em")
+              .style("text-anchor", "middle")
+              .text(function(d) {
+                  return d.data.name + ": " + d.data.times;
+              });
+
+      d3.select(self.frameElement)
+              .style("height", height + "px");
+}
