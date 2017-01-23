6b6f56bfd7a8223bccef18f0b5f1b1d7d9fd22e2
MerelSteenbrink <merelsteenbrink@gmail.com>
2017-01-23 17:21:26 +0100

    works for d3 version 4

---
+++ donut/script.js
@@ -0,0 +1,42 @@
+
+var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scaleOrdinal()
+    .range([ "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(radius - 70);
+
+var pie = d3.pie()
+    .sort(null)
+    .value(function(d) { 
+      console.log(d.buzz)
+      return d.buzz; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+d3.json("data.json", function(error, data) {
+  if (error) throw error;
+console.log(data)
+  var g = svg.selectAll(".arc")
+      .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.name); });
+
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function(d) { return d.data.name; });
+});
+
